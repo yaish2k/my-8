96df5063f2cb9b790d0655e4e2351bc4b95b5ab0
gal_yaish <yaish2k@gmail.com>
2019-11-08 18:33:37 +0200

    remove import

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,7 +1,5 @@
 const admin = require('firebase-admin');
 const config = require('../config/index');
-var serviceAccount = require('../config/vars/firebase-credentials.json');
-
 
 admin.initializeApp({
     credential: admin.credential.cert(config.firebase.credentials),
